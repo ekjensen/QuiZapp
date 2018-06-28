02417351ac9ce38d7aa75992b34126fd99dd35a0
Emily Jensen <emilykathleenjensen@gmail.com>
2018-06-28 18:21:26 -0400

    Adds quesions property and quiznames properties

---
+++ Models/Questions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuiZapp.Models
+{
+    public class Questions
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID { get; set; }
+        public string QuestionText { get; set; }
+        public int QuizID { get; set; }
+
+        public virtual ICollection<QuizName> QuizNames { get; set; }
+    }
+}
