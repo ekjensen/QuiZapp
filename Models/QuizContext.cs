38b5ba209c0f88fb1ed7c3a9e2b0fa75cfad84e7
Emily Jensen <emilykathleenjensen@gmail.com>
2018-06-29 11:14:41 -0400

    Adds data model

---
+++ Models/QuizContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace QuiZapp.Models
+{
+    public class QuizContext : DbContext
+    {
+        public QuizContext(DbContextOptions<QuizContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Quiz> Quiz { get; set; }
+        public DbSet<Question> Question { get; set; }
+        public DbSet<QuizAnswer> QuizAnswer { get; set; }
+        public DbSet<Result> Result { get; set; }
+        public DbSet<UserAnswer> UserAnswer { get; set; }
+        public DbSet<User> User { get; set; }
+    }
+}
